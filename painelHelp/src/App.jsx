b51536d57b443ad4e8b8de61c2ae2b757197ad0e
lucasccgomes <lucasccgomes@protonmail.com>
2024-07-02 10:07:22 -0300

    feat:update for route protection

--- painelHelp/src/App.jsx
+++ painelHelp/src/App.jsx
@@ -3,28 +3,39 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/NavBar';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider, useAuth } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 
-function App() {
+const App = () => {
   return (
     <AuthProvider>
       <Router>
-        <Navbar />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <HomePage />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
+        <AuthConsumer>
+          {({ isAuthenticated }) => (
+            <>
+              {isAuthenticated && <Navbar />}
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <ProtectedRoute>
+                      <HomePage />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route path="/login" element={<LoginPage />} />
+              </Routes>
+            </>
+          )}
+        </AuthConsumer>
       </Router>
     </AuthProvider>
   );
-}
+};
+
+const AuthConsumer = ({ children }) => {
+  const authContext = useAuth();
+  return children(authContext);
+};
 
 export default App;
