678945b6b1f15d954ff8859d0510600fc7e48696
lucasccgomes <lucasccgomes@protonmail.com>
2024-07-04 15:49:09 -0300

    feat:update style

--- painelTi/tailwind.config.js
+++ painelTi/tailwind.config.js
@@ -2,10 +2,20 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#002d56',
+        secondary: '#004080',
+        tertiary: '#0066cc',
+        accent: '#3399ff',
+        primaryOpaci: '#145996',
+        'background-light': '#e6f2ff',
+        'background-dark': '#001a33',
+      },
+    },
   },
   plugins: [],
 }
