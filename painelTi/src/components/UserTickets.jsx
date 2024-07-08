3d21b0ca53fdfaf6308de1bc58950a80829025bd
lucasccgomes <lucasccgomes@protonmail.com>
2024-07-08 17:13:21 -0300

    feat:change nbo style to carousel

--- painelTi/src/components/UserTickets.jsx
+++ painelTi/src/components/UserTickets.jsx
@@ -5,6 +5,8 @@ import { db } from '../firebase';
 import { useAuth } from '../context/AuthContext';
 import { FaCity, FaUser, FaStoreAlt } from "react-icons/fa";
 import { MdReportProblem } from "react-icons/md";
+import { useSwipeable } from 'react-swipeable';
+import { GrCaretPrevious, GrCaretNext } from "react-icons/gr";
 
 const UserTickets = () => {
   const { currentUser } = useAuth();
@@ -17,6 +19,8 @@ const UserTickets = () => {
   const [finalizadoDescricao, setFinalizadoDescricao] = useState('');
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [selectedImage, setSelectedImage] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [itemsPerSlide, setItemsPerSlide] = useState(3);
 
   const getStatusClass = (status) => {
     switch (status) {
@@ -54,6 +58,46 @@ const UserTickets = () => {
 
     fetchTickets();
   }, []);
+
+  useEffect(() => {
+    const updateItemsPerSlide = () => {
+      if (window.innerWidth >= 1280) {
+        setItemsPerSlide(3);
+      } else if (window.innerWidth >= 1023) {
+        setItemsPerSlide(3);
+      } else {
+        setItemsPerSlide(1);
+      }
+    };
+
+    window.addEventListener('resize', updateItemsPerSlide);
+    updateItemsPerSlide();
+
+    return () => window.removeEventListener('resize', updateItemsPerSlide);
+  }, []);
+
+  const addTicket = (ticket) => {
+    setTickets((prevTickets) => [ticket, ...prevTickets]);
+  };
+
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? Math.ceil(tickets.length / itemsPerSlide) - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToNext = () => {
+    const isLastSlide = currentIndex === Math.ceil(tickets.length / itemsPerSlide) - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const handlers = useSwipeable({
+    onSwipedLeft: goToNext,
+    onSwipedRight: goToPrevious,
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true
+  });
 
   const updateTicketStatus = async (id, status, descricaoFinalizacao = '') => {
     try {
@@ -95,8 +139,8 @@ const UserTickets = () => {
   };
 
   return (
-    <div className="p-4 flex flex-col justify-center items-center">
-      <div className='flex lg:flex-row flex-col gap-4 items-center mb-4'>
+    <div className="p-4 w-screen flex flex-col justify-center items-center">
+     <div className='flex flex-col lg:flex-row gap-4'>
         <h2 className="text-2xl font-bold">Chamados</h2>
 
         <div className=''>
@@ -136,96 +180,104 @@ const UserTickets = () => {
       {filteredTickets.length === 0 ? (
         <p>Nenhum chamado encontrado.</p>
       ) : (
-        <ul className='max-w-[500px]'>
-          {filteredTickets.map(ticket => (
-            <li key={ticket.id} className="bg-slate-400 shadow-xl mb-4 p-4 border rounded-xl">
-              <h3 className="text-xl uppercase text-center font-semibold">
-                {ticket.order}
-                <p className={`my-1 p-1 rounded-lg text-white uppercase ${getStatusClass(ticket.status)}`}>
-                  {ticket.status}
-                </p>
-              </h3>
+        <div className="relative p-4 w-screen" {...handlers}>
+          <div className="overflow-hidden">
+            <div className="flex transition-transform duration-500" style={{ transform: `translateX(-${currentIndex * 100 / itemsPerSlide}%)` }}>
+              {filteredTickets.map(ticket => (
+                <div key={ticket.id} className='gap-4' style={{ flex: `0 0 ${100 / itemsPerSlide}%` }}>
+                  <div className="bg-gray-400 shadow-xl lg:min-w-[250px] mb-4 p-4 border-2 rounded">
+                    <h3 className="text-xl uppercase text-center font-semibold">
+                      {ticket.order}
+                      <p className={`my-1 p-1 rounded-lg text-white uppercase ${getStatusClass(ticket.status)}`}>
+                        {ticket.status}
+                      </p>
+                    </h3>
 
-              <div className='flex gap-4 mb-1'>
-                <p className='flex uppercase items-center'><FaCity />: {ticket.cidade}</p>
-                <p className='flex uppercase items-center'><FaUser />: {ticket.user}</p>
-              </div>
+                    <div className='flex gap-4 mb-1'>
+                      <p className='flex uppercase items-center'><FaCity />: {ticket.cidade}</p>
+                      <p className='flex uppercase items-center'><FaUser />: {ticket.user}</p>
+                    </div>
 
-              <div className='flex gap-4 mb-1'>
-                <p className='flex uppercase items-center'><FaStoreAlt />: {ticket.loja}</p>
-                <p className='flex uppercase items-center'>
-                  <MdReportProblem />: {ticket.localProblema}
-                </p>
-              </div>
+                    <div className='flex gap-4 mb-1'>
+                      <p className='flex uppercase items-center'><FaStoreAlt />: {ticket.loja}</p>
+                      <p className='flex uppercase items-center'>
+                        <MdReportProblem />: {ticket.localProblema}
+                      </p>
+                    </div>
 
-              <div className='flex gap-4 mb-1'>
-                <p>
-                  <strong>
-                    Data:
-                  </strong>
-                  {ticket.data.toLocaleString()}
-                </p>
-              </div>
+                    <div className='flex gap-4 mb-1'>
+                      <p>
+                        <strong>
+                          Data:
+                        </strong>
+                        {ticket.data.toLocaleString()}
+                      </p>
+                    </div>
 
-              <div className='bg-white p-3 rounded-md mb-2 mt-2'>
-                <p className='text-center font-bold'>Descrição</p>
-                <p>{ticket.descricao}</p>
-              </div>
+                    <div className='bg-white p-3 rounded-md max-h-20 overflow-y-auto mb-2 mt-2'>
+                      <p className='text-center font-bold'>Descrição</p>
+                      <p>{ticket.descricao}</p>
+                    </div>
 
-              <div className='bg-white p-3 rounded-md mb-2 mt-2'>
-                <p className='text-center font-bold'>Tentativa</p>
-                <p>{ticket.tentou}</p>
-              </div>
+                    <div className='bg-white p-3 rounded-md max-h-20 overflow-y-auto mb-2 mt-2'>
+                      <p className='text-center font-bold'>Tentativa</p>
+                      <p>{ticket.tentou}</p>
+                    </div>
 
-              {Array.isArray(ticket.imgUrl) && ticket.imgUrl.length > 0 && (
-                <div className='bg-white p-3 rounded-md mb-2 mt-2'>
-                  <p className='text-center font-bold'>Imagens</p>
-                  <div className='flex gap-2'>
-                    {ticket.imgUrl.map((image, index) => (
-                      <img
-                        key={index}
-                        src={image}
-                        alt={`ticket-${index}`}
-                        className='w-20 h-20 object-cover cursor-pointer'
-                        onClick={() => openImageModal(image)}
-                      />
-                    ))}
+                    {Array.isArray(ticket.imgUrl) && ticket.imgUrl.length > 0 && (
+                      <div className='bg-white p-3 rounded-md mb-2 mt-2'>
+                        <p className='text-center font-bold'>Imagens</p>
+                        <div className='flex gap-2'>
+                          {ticket.imgUrl.map((image, index) => (
+                            <img
+                              key={index}
+                              src={image}
+                              alt={`ticket-${index}`}
+                              className='w-16 h-16 object-cover cursor-pointer'
+                              onClick={() => openImageModal(image)}
+                            />
+                          ))}
+                        </div>
+                      </div>
+                    )}
+
+                    {ticket.descricaoFinalizacao && (
+                      <p className='bg-blue-100 p-3 rounded-md mt-2 max-h-40 overflow-y-auto'>
+                        <strong>Conclusão:</strong>
+                        {ticket.descricaoFinalizacao}
+                      </p>
+                    )}
+                    <div className="flex gap-2 mt-2">
+                      <button
+                        onClick={() => updateTicketStatus(ticket.id, 'aberto')}
+                        className={`bg-red-400 text-white px-4 py-2 rounded ${ticket.status === 'finalizado' ? 'opacity-50 cursor-not-allowed' : ''} ${ticket.status === 'aberto' ? 'bg-red-600 ring-2 ring-white' : ''}`}
+                        disabled={ticket.status === 'finalizado'}
+                      >
+                        Aberto
+                      </button>
+                      <button
+                        onClick={() => updateTicketStatus(ticket.id, 'andamento')}
+                        className={`bg-orange-400 text-white px-4 py-2 rounded ${ticket.status === 'finalizado' ? 'opacity-50 cursor-not-allowed' : ''} ${ticket.status === 'andamento' ? 'bg-orange-600 ring-2 ring-white' : ''}`}
+                        disabled={ticket.status === 'finalizado'}
+                      >
+                        Andamento
+                      </button>
+                      <button
+                        onClick={() => setSelectedTicket(ticket)}
+                        className={`bg-green-400 text-white px-4 py-2 rounded ${ticket.status === 'finalizado' ? 'opacity-50 cursor-not-allowed' : ''} ${ticket.status === 'finalizado' ? 'bg-green-600 ring-2 ring-white' : ''}`}
+                        disabled={ticket.status === 'finalizado'}
+                      >
+                        Finalizado
+                      </button>
+                    </div>
                   </div>
                 </div>
-              )}
-
-              {ticket.descricaoFinalizacao && (
-                <p className='bg-blue-100 p-3 rounded-md mt-2'>
-                  <strong>Conclusão:</strong>
-                  {ticket.descricaoFinalizacao}
-                </p>
-              )}
-              <div className="flex gap-2 mt-2">
-                <button
-                  onClick={() => updateTicketStatus(ticket.id, 'aberto')}
-                  className={`bg-red-400 text-white px-4 py-2 rounded ${ticket.status === 'finalizado' ? 'opacity-50 cursor-not-allowed' : ''} ${ticket.status === 'aberto' ? 'bg-red-600 ring-2 ring-white' : ''}`}
-                  disabled={ticket.status === 'finalizado'}
-                >
-                  Aberto
-                </button>
-                <button
-                  onClick={() => updateTicketStatus(ticket.id, 'andamento')}
-                  className={`bg-orange-400 text-white px-4 py-2 rounded ${ticket.status === 'finalizado' ? 'opacity-50 cursor-not-allowed' : ''} ${ticket.status === 'andamento' ? 'bg-orange-600 ring-2 ring-white' : ''}`}
-                  disabled={ticket.status === 'finalizado'}
-                >
-                  Andamento
-                </button>
-                <button
-                  onClick={() => setSelectedTicket(ticket)}
-                  className={`bg-green-400 text-white px-4 py-2 rounded ${ticket.status === 'finalizado' ? 'opacity-50 cursor-not-allowed' : ''} ${ticket.status === 'finalizado' ? 'bg-green-600 ring-2 ring-white' : ''}`}
-                  disabled={ticket.status === 'finalizado'}
-                >
-                  Finalizado
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
+              ))}
+            </div>
+          </div>
+          <button onClick={goToPrevious} className="absolute top-1/2 left-5 transform -translate-y-1/2 bg-primary text-white p-3 rounded opacity-85"><GrCaretPrevious /></button>
+          <button onClick={goToNext} className="absolute top-1/2 right-5 transform -translate-y-1/2 bg-primary text-white p-3 rounded opacity-85"><GrCaretNext /></button>
+        </div>
       )}
 
       {selectedTicket && (
