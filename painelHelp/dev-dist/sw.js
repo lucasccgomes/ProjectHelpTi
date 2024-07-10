1277e29156425798fac3eaa32f63ff2acca7a418
lucasccgomes <lucasccgomes@protonmail.com>
2024-07-10 17:19:08 -0300

    feat:config pwa

--- painelHelp/dev-dist/sw.js
+++ painelHelp/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.u24ets8ceqg"
+    "revision": "0.fhiobjqv5io"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
