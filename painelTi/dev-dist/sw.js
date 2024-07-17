f85bbf86817b09aa85b083171325668cd880b32f
lucasccgomes <lucasccgomes@protonmail.com>
2024-07-17 16:48:33 -0300

    config

--- painelTi/dev-dist/sw.js
+++ painelTi/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qsku0mv3euo"
+    "revision": "0.jahvteurqtg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
