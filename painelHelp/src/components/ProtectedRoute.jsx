773557b94c2b21646d1cdbaa7e3de69f0fc066fb
lucasccgomes <lucasccgomes@protonmail.com>
2024-07-17 16:45:35 -0300

    feat:screen permission setting

--- painelHelp/src/components/ProtectedRoute.jsx
+++ painelHelp/src/components/ProtectedRoute.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
-const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated, loading } = useAuth();
+const ProtectedRoute = ({ children, allowedRoles }) => {
+  const { isAuthenticated, loading, currentUserRole } = useAuth();
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
-  return isAuthenticated ? children : <Navigate to="/login" />;
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  if (allowedRoles && !allowedRoles.includes(currentUserRole)) {
+    return <Navigate to="/nopermission" />;
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
