af407cae33f4ce8d36debcc6b417e9ca8b80574e
lucasccgomes <lucasccgomes@protonmail.com>
2024-07-19 17:38:14 -0300

    feat:improvement in dropdown and select component

--- painelHelp/src/pages/UserTickets.jsx
+++ painelHelp/src/pages/UserTickets.jsx
@@ -7,6 +7,7 @@ import { FaCity, FaUser, FaStoreAlt } from "react-icons/fa";
 import { MdReportProblem, MdHelp } from "react-icons/md";
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Dropdown from '../components/Dropdown/Dropdown';
 
 const UserTickets = () => {
   const { currentUser } = useAuth();
@@ -14,18 +15,20 @@ const UserTickets = () => {
   const [loading, setLoading] = useState(true);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [slidesToShow, setSlidesToShow] = useState(3);
-  const [statusFilter, setStatusFilter] = useState('aberto');
+  const [statusFilter, setStatusFilter] = useState('Aberto');
   const [showArrows, setShowArrows] = useState(false);
 
 
   const getStatusClass = (status) => {
     switch (status) {
-      case 'aberto':
+      case 'Aberto':
         return 'bg-red-600';
-      case 'andamento':
+      case 'Andamento':
         return 'bg-orange-600';
-      case 'finalizado':
+      case 'Finalizado':
         return 'bg-green-600';
+      case 'VSM':
+        return 'bg-blue-700';
       default:
         return 'bg-gray-700';
     }
@@ -37,7 +40,7 @@ const UserTickets = () => {
         let abertoRef = collection(db, 'chamados', 'aberto', 'tickets');
         let q = query(abertoRef, where('user', '==', currentUser));
 
-        if (statusFilter !== 'todos') {
+        if (statusFilter !== 'Todos') {
           q = query(abertoRef, where('user', '==', currentUser), where('status', '==', statusFilter));
         }
 
@@ -89,9 +92,9 @@ const UserTickets = () => {
   }
 
   return (
-    <div className="p-4 w-full pt-20 flex flex-col justify-center items-center">
+    <div className="p-4 w-full pt-20 flex flex-col justify-center items-center text-gray-700">
       <div className='flex flex-col lg:flex-row gap-4'>
-        <h2 className="text-2xl font-bold mb-4">Meus Chamados</h2>
+        <h2 className="text-2xl font-bold mb-4 text-gray-700">Meus Chamados</h2>
         <button
           className="mb-4 bg-primary flex justify-center items-center hover:bg-secondary text-white font-bold py-2 px-4 rounded"
           onClick={() => setIsModalOpen(true)}
@@ -101,21 +104,17 @@ const UserTickets = () => {
         <NewTicketModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} addTicket={addTicket} />
       </div>
 
-      <div className="mb-4">
-        <label htmlFor="statusFilter" className="mr-2 font-bold">Filtrar por status:</label>
-        <select
-          id="statusFilter"
-          value={statusFilter}
-          onChange={(e) => setStatusFilter(e.target.value)}
-          className="border p-2 rounded"
-        >
-          <option value="todos">Todos</option>
-          <option value="aberto">Aberto</option>
-          <option value="andamento">Andamento</option>
-          <option value="finalizado">Finalizado</option>
-        </select>
+      <div className="flex-row w-full mb-8">
+        <p className='-mb-2'>
+          Filtrar
+        </p>
+        <Dropdown
+          label=""
+          options={['Todos', 'Aberto', 'Andamento', 'Finalizado', 'VSM']}
+          selected={statusFilter}
+          onSelectedChange={(option) => setStatusFilter(option)}
+        />
       </div>
-
       {tickets.length === 0 ? (
         <p>Nenhum chamado em seu nome.</p>
       ) : (
@@ -136,13 +135,13 @@ const UserTickets = () => {
           >
             {tickets.map(ticket => (
               <div key={ticket.id} className='gap-4'>
-                <div className="bg-gray-400 shadow lg:min-w-[250px] mb-4 p-4 border-2 rounded">
-                  <h3 className="text-xl uppercase text-center font-semibold">
+                <div className="bg-primary text-white shadow lg:min-w-[250px] mb-4 p-4 border-2 rounded-xl">
+                  <div className="text-xl text-gray-700 bg-white font-bold uppercase rounded-b-xl -mt-5 mb-2 pb-1 text-center">
                     {ticket.order}
-                    <p className={`my-1 p-1 rounded-lg text-white uppercase ${getStatusClass(ticket.status)}`}>
-                      {ticket.status}
-                    </p>
-                  </h3>
+                  </div>
+                  <p className={`my-1 p-1 rounded-lg text-white uppercase shadow-xl ${getStatusClass(ticket.status)}`}>
+                    {ticket.status}
+                  </p>
 
                   <div className='flex gap-4 mb-1'>
                     <p className='flex uppercase items-center'><FaCity />: {ticket.cidade}</p>
@@ -164,11 +163,11 @@ const UserTickets = () => {
                       {ticket.data.toLocaleString()}
                     </p>
                   </div>
-                  <div className='bg-white pt-0 px-2 pb-1 rounded-md mb-2'>
+                  <div className='bg-white text-gray-700 pt-0 px-2 pb-1 rounded-md mb-2 shadow-lg'>
                     <p className='text-center font-bold'>Descrição</p>
                     <p className=' overflow-y-auto break-words max-h-14 min-h-14' >{ticket.descricao}</p>
                   </div>
-                  <div className='bg-white pt-0 px-2 pb-1 rounded-md '>
+                  <div className='bg-white text-gray-700 pt-0 px-2 pb-1 rounded-md shadow-lg'>
                     <p className='text-center font-bold'>Tentativa</p>
                     <p className=' overflow-y-auto break-words max-h-14 min-h-14' >{ticket.tentou}</p>
                   </div>
