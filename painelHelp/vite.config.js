b706e524e984f7617cf559c7761a09a699245c75
lucasccgomes <lucasccgomes@protonmail.com>
2024-07-24 10:15:42 -0300

    feat:update config

--- painelHelp/vite.config.js
+++ painelHelp/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+//vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
       devOptions: {
-        enabled: true
+        enabled: true,
       },
       manifest: {
         id: "/",
@@ -25,49 +25,116 @@ export default defineConfig({
           {
             src: 'icon/android-launchericon-48-48.png',
             sizes: '48x48',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'icon/android-launchericon-72-72.png',
             sizes: '72x72',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'icon/android-launchericon-96-96.png',
             sizes: '96x96',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'icon/android-launchericon-144-144.png',
             sizes: '144x144',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'icon/android-launchericon-192-192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'icon/android-launchericon-512-512.png',
             sizes: '512x512',
-            type: 'image/png'
-          }
+            type: 'image/png',
+          },
         ],
         screenshots: [
           {
-            src: 'screenshots/screenshot-1.png',
+            src: 'screenshot/01.png',
             sizes: '1440x900',
             type: 'image/png',
-            form_factor: 'wide'
+            form_factor: 'wide',
           },
           {
-            src: "screenshots/screenshot-2.png",
-            sizes: "398x712",
-            type: "image/png",
-            form_factor: "narrow"
-          }
-        ]
-      }
-    })
-  ]
+            src: 'screenshot/02.png',
+            sizes: '398x712',
+            type: 'image/png',
+            form_factor: 'narrow',
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: '/index.html',
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'gstatic-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/cdn.jsdelivr.net\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'jsdelivr-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+              },
+            },
+          },
+          {
+            urlPattern: /\/.*\.(?:js|css|html|png|jpg|jpeg|svg|gif)/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'static-resources',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+              },
+            },
+          },
+          {
+            urlPattern: ({ request }) => request.mode === 'navigate',
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'navigations',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
 });
