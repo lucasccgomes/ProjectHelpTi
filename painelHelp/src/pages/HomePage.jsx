ecb2579bc02bec87718bed6e80f458cd5c987498
lucasccgomes <lucasccgomes@protonmail.com>
2024-07-30 17:33:04 -0300

    update

--- painelHelp/src/pages/HomePage.jsx
+++ painelHelp/src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useAuth } from '../context/AuthContext';
+import NotificationModal from '../components/NotificationModal/NotificationModal';
 
 const HomePage = () => {
   const { currentUserRole } = useAuth();
 
   return (
     <div className="pt-20 min-h-screen flex flex-col items-center justify-center bg-gray-100">
-    
+     <NotificationModal />
     </div>
   );
 };
