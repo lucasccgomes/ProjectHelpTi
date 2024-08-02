4169846fd5ea744d1e3d0508b3fd8790c63c6b32
lucasccgomes <lucasccgomes@protonmail.com>
2024-08-02 10:36:44 -0300

    feat:animation in modal

--- painelHelp/src/components/Modal/Modal.jsx
+++ painelHelp/src/components/Modal/Modal.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import { useSpring, animated } from '@react-spring/web';
 
 const Modal = ({ isOpen, onClose, children }) => {
+  const animation = useSpring({
+    opacity: isOpen ? 1 : 0,
+    transform: isOpen ? 'translateY(0)' : 'translateY(-100%)',
+  });
+
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 z-50 overflow-auto bg-smoke-light flex">
-      <div className="relative p-8 bg-white w-full max-w-md m-auto flex-col flex rounded-lg shadow-lg">
+      <animated.div style={animation} className="relative p-8 bg-white w-full max-w-md m-auto flex-col flex rounded-lg shadow-lg">
         <span onClick={onClose} className="cursor-pointer absolute top-0 w-10 flex justify-center items-center h-10 right-0 p-4 b bg-red-600 rounded-full m-2">
-          <button className='text-white font-bold' >X</button>
+          <button className='text-white font-bold'>X</button>
         </span>
         {children}
-      </div>
-
+      </animated.div>
     </div>
   );
 };
