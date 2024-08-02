4600ba944cf10f7a2f8361072414fad0f9a14ae3
lucasccgomes <lucasccgomes@protonmail.com>
2024-08-02 10:38:15 -0300

    feat:add interface with condition linked to position

--- painelHelp/src/pages/UserTickets.jsx
+++ painelHelp/src/pages/UserTickets.jsx
@@ -1,23 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import { collection, query, where, onSnapshot } from 'firebase/firestore';
+import { collection, query, where, onSnapshot, doc, getDoc, getDocs } from 'firebase/firestore';
 import { db } from '../firebase';
 import { useAuth } from '../context/AuthContext';
 import NewTicketModal from '../components/NewTicketModal/NewTicketModal';
-import { FaCity, FaUser, FaStoreAlt } from "react-icons/fa";
+import { FaCity, FaUser, FaStoreAlt, FaFilter } from "react-icons/fa";
+import { IoCalendarNumber } from "react-icons/io5";
 import { MdReportProblem, MdHelp } from "react-icons/md";
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Dropdown from '../components/Dropdown/Dropdown';
 import NotificationModal from '../components/NotificationModal/NotificationModal';
+import Modal from '../components/Modal/Modal';
 
 const UserTickets = () => {
   const { currentUser } = useAuth();
   const [tickets, setTickets] = useState([]);
   const [loading, setLoading] = useState(true);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isFilterModalOpen, setIsFilterModalOpen] = useState(false);
   const [slidesToShow, setSlidesToShow] = useState(3);
   const [statusFilter, setStatusFilter] = useState('Todos');
+  const [cityFilter, setCityFilter] = useState('Todas');
+  const [storeFilter, setStoreFilter] = useState('Todas');
+  const [userFilter, setUserFilter] = useState('Todos');
   const [showArrows, setShowArrows] = useState(false);
+  const [cities, setCities] = useState([]);
+  const [stores, setStores] = useState([]);
+  const [users, setUsers] = useState([]);
 
   const getStatusClass = (status) => {
     switch (status) {
@@ -35,12 +44,60 @@ const UserTickets = () => {
   };
 
   useEffect(() => {
+    const fetchCitiesAndStores = async () => {
+      const citiesDoc = await getDoc(doc(db, 'ordersControl', 'cidades'));
+      if (citiesDoc.exists()) {
+        const citiesData = citiesDoc.data();
+        const cityNames = Object.keys(citiesData);
+
+        setCities(cityNames);
+
+        if (cityFilter !== 'Todas') {
+          setStores(citiesData[cityFilter]);
+        } else {
+          const allStores = cityNames.flatMap(city => citiesData[city]);
+          setStores(allStores);
+        }
+      }
+    };
+
+    fetchCitiesAndStores();
+  }, [cityFilter]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const ticketsSnapshot = await getDocs(collection(db, 'chamados', 'aberto', 'tickets'));
+      const usersSet = new Set();
+
+      ticketsSnapshot.forEach(doc => {
+        const data = doc.data();
+        if (data.user) {
+          usersSet.add(data.user);
+        }
+      });
+
+      setUsers(Array.from(usersSet));
+    };
+
+    fetchUsers();
+  }, []);
+
+  useEffect(() => {
     const fetchTickets = () => {
       let abertoRef = collection(db, 'chamados', 'aberto', 'tickets');
-      let q = query(abertoRef, where('user', '==', currentUser.user));
+      let q;
 
-      if (statusFilter !== 'Todos') {
-        q = query(abertoRef, where('user', '==', currentUser.user), where('status', '==', statusFilter));
+      if (currentUser.cargo === 'Supervisor') {
+        q = query(abertoRef);
+        if (cityFilter !== 'Todas') q = query(q, where('cidade', '==', cityFilter));
+        if (storeFilter !== 'Todas') q = query(q, where('loja', '==', storeFilter));
+        if (userFilter !== 'Todos') q = query(q, where('user', '==', userFilter));
+        if (statusFilter !== 'Todos') q = query(q, where('status', '==', statusFilter));
+      } else {
+        q = query(abertoRef, where('user', '==', currentUser.user));
+        if (statusFilter !== 'Todos') {
+          q = query(q, where('status', '==', statusFilter));
+        }
       }
 
       const unsubscribe = onSnapshot(q, (querySnapshot) => {
@@ -66,7 +123,7 @@ const UserTickets = () => {
     if (currentUser.user) {
       fetchTickets();
     }
-  }, [currentUser.user, statusFilter]);
+  }, [currentUser, statusFilter, cityFilter, storeFilter, userFilter]);
 
   useEffect(() => {
     const handleResize = () => {
@@ -99,28 +156,136 @@ const UserTickets = () => {
 
   return (
     <div className="p-4 w-full pt-20 flex flex-col justify-center items-center text-gray-700">
-      <div className='flex flex-col lg:flex-row gap-4'>
-        <h2 className="text-2xl font-bold mb-4 text-gray-700">Meus Chamados</h2>
-        <button
-          className="mb-4 bg-primary flex justify-center items-center hover:bg-secondary text-white font-bold py-2 px-4 rounded"
-          onClick={() => setIsModalOpen(true)}
-        >
-          <MdHelp className='text-xl mr-1' /> Novo Chamado
-        </button>
+      <div className='flex flex-col lg:flex-row gap-4 w-full items-center mb-4'>
+        <h2 className="text-2xl w-full font-bold mb-1 text-center lg:text-end text-gray-700">Meus Chamados</h2>
+        <div className="flex w-full items-center lg:justify-start justify-center gap-2 ">
+          <button
+            className=" bg-primary flex justify-center items-center hover:bg-secondary text-white font-bold py-2 px-4 rounded"
+            onClick={() => setIsModalOpen(true)}
+          >
+            <MdHelp className='text-xl mr-1' /> Novo Chamado
+          </button>
+          <button
+            className=" lg:hidden bg-primary flex justify-center items-center hover:bg-secondary text-white font-bold py-2 px-4 rounded"
+            onClick={() => setIsFilterModalOpen(true)}
+          >
+            <FaFilter className='text-xl' /> Filtrar
+          </button>
+        </div>
+
         <NewTicketModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} addTicket={addTicket} />
+
       </div>
 
-      <div className="flex-row w-full mb-8">
-        <p className='-mb-2'>
-          Filtrar
-        </p>
-        <Dropdown
-          label=""
-          options={['Todos', 'Aberto', 'Andamento', 'Finalizado', 'VSM']}
-          selected={statusFilter}
-          onSelectedChange={(option) => setStatusFilter(option)}
-        />
+
+
+      <div className="hidden bg-primary p-3 rounded-xl text-white lg:flex flex-row w-full mb-8 justify-center items-center text-center gap-4">
+        <div className='flex justify-center items-center'>
+          <FaFilter /><p className=''>Filtrar</p>
+        </div>
+        {currentUser.cargo === 'Supervisor' && (
+          <>
+            <div className='flex flex-col min-w-36'>
+              <p className='-mb-2'>
+                Cidade
+              </p>
+              <Dropdown
+                label=""
+                options={['Todas', ...cities]}
+                selected={cityFilter}
+                onSelectedChange={(option) => setCityFilter(option)}
+              />
+            </div>
+            <div className='flex flex-col min-w-28'>
+              <p className='-mb-2'>
+                Loja
+              </p>
+              <Dropdown
+                label=""
+                options={['Todas', ...stores]}
+                selected={storeFilter}
+                onSelectedChange={(option) => setStoreFilter(option)}
+              />
+            </div>
+            <div className='flex flex-col min-w-28'>
+              <p className='-mb-2'>
+                Usuário
+              </p>
+              <Dropdown
+                label=""
+                options={['Todos', ...users]}
+                selected={userFilter}
+                onSelectedChange={(option) => setUserFilter(option)}
+              />
+            </div>
+          </>
+        )}
+        <div className='flex flex-col min-w-28'>
+          <p className='-mb-2'>
+            Status
+          </p>
+          <Dropdown
+            label=""
+            options={['Todos', 'Aberto', 'Andamento', 'Finalizado', 'VSM']}
+            selected={statusFilter}
+            onSelectedChange={(option) => setStatusFilter(option)}
+          />
+        </div>
       </div>
+
+      <Modal isOpen={isFilterModalOpen} onClose={() => setIsFilterModalOpen(false)}>
+        <div className="flex flex-col w-full mb-8">
+          {currentUser.cargo === 'Supervisor' && (
+            <>
+              <p className='-mb-2'>
+                Filtrar por Cidade
+              </p>
+              <Dropdown
+                label=""
+                options={['Todas', ...cities]}
+                selected={cityFilter}
+                onSelectedChange={(option) => setCityFilter(option)}
+              />
+
+              <p className='-mb-2'>
+                Filtrar por Loja
+              </p>
+              <Dropdown
+                label=""
+                options={['Todas', ...stores]}
+                selected={storeFilter}
+                onSelectedChange={(option) => setStoreFilter(option)}
+              />
+
+              <p className='-mb-2'>
+                Filtrar por Usuário
+              </p>
+              <Dropdown
+                label=""
+                options={['Todos', ...users]}
+                selected={userFilter}
+                onSelectedChange={(option) => setUserFilter(option)}
+              />
+            </>
+          )}
+          <p className='-mb-2'>
+            Filtrar por Status
+          </p>
+          <Dropdown
+            label=""
+            options={['Todos', 'Aberto', 'Andamento', 'Finalizado', 'VSM']}
+            selected={statusFilter}
+            onSelectedChange={(option) => setStatusFilter(option)}
+          />
+        </div>
+        <button
+          className="bg-primary flex justify-center items-center hover:bg-secondary text-white font-bold py-2 px-4 rounded"
+          onClick={() => setIsFilterModalOpen(false)}
+        >
+          Filtrar
+        </button>
+      </Modal>
+
       {tickets.length === 0 ? (
         <p>Nenhum chamado em seu nome.</p>
       ) : (
@@ -162,11 +327,8 @@ const UserTickets = () => {
                   </div>
 
                   <div className='flex gap-4 mb-1'>
-                    <p>
-                      <strong>
-                        Data:
-                      </strong>
-                      {ticket.data.toLocaleString()}
+                    <p className='flex justify-center items-center'>
+                      <IoCalendarNumber />: {ticket.data.toLocaleString()}
                     </p>
                   </div>
                   <div className='bg-white text-gray-700 pt-0 px-2 pb-1 rounded-md mb-2 shadow-lg'>
@@ -174,8 +336,12 @@ const UserTickets = () => {
                     <p className=' overflow-y-auto break-words max-h-14 min-h-14' >{ticket.descricao}</p>
                   </div>
                   <div className='bg-white text-gray-700 pt-0 px-2 pb-1 rounded-md shadow-lg'>
-                    <p className='text-center font-bold'>Tentativa</p>
-                    <p className=' overflow-y-auto break-words max-h-14 min-h-14' >{ticket.tentou}</p>
+                    <p className='text-center font-bold'>
+                      {ticket.status === 'Andamento' ? 'Tratativa' : 'Tentativa'}
+                    </p>
+                    <p className='overflow-y-auto break-words max-h-14 min-h-14'>
+                      {ticket.status === 'Andamento' ? ticket.treatment : ticket.tentou}
+                    </p>
                   </div>
                 </div>
               </div>
