01afb1d89f5d4d13192c873b4748936a72b71ca6
lucasccgomes <lucasccgomes@protonmail.com>
2024-08-16 08:12:08 -0300

    feat:animate

--- painelTi/src/components/Modal/MyModal.jsx
+++ painelTi/src/components/Modal/MyModal.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
-import { useSpring, animated } from '@react-spring/web';
+import { useTransition, animated } from '@react-spring/web';
 
 const MyModal = ({ isOpen, onClose, children }) => {
-  const animation = useSpring({
-    opacity: isOpen ? 1 : 0,
-    transform: isOpen ? 'translateY(0)' : 'translateY(-100%)',
+  const transitions = useTransition(isOpen, {
+    from: { opacity: 0, transform: 'translateY(-50%)' },
+    enter: { opacity: 1, transform: 'translateY(0%)' },
+    leave: { opacity: 0, transform: 'translateY(-50%)' },
   });
 
-  if (!isOpen) return null;
-
-  return (
-    <div className="fixed inset-0 z-50 overflow-auto bg-smoke-light bg-black/[.6] flex">
-      <animated.div style={animation} className="relative p-8 bg-white w-full max-w-md m-auto flex-col flex rounded-lg shadow-lg">
-        <span onClick={onClose} className="cursor-pointer absolute top-0 w-10 flex justify-center items-center h-10 right-0 p-4 b bg-red-600 rounded-full m-2">
-          <button className='text-white font-bold'>X</button>
-        </span>
-        {children}
-      </animated.div>
-    </div>
+  return transitions(
+    (styles, item) =>
+      item && (
+        <div className="fixed inset-0 z-50 overflow-auto bg-smoke-light bg-black/[.6] flex">
+          <animated.div style={styles} className="relative p-8 bg-white w-full max-w-md m-auto flex-col flex rounded-lg shadow-lg">
+            <span
+              onClick={onClose}
+              className="cursor-pointer absolute top-0 w-10 flex justify-center items-center h-10 right-0 p-4 bg-red-600 rounded-full m-2"
+            >
+              <button className="text-white font-bold">X</button>
+            </span>
+            {children}
+          </animated.div>
+        </div>
+      )
   );
 };
 
