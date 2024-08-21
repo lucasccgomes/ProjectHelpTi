f04dd30589ec4bcee4e42db27b24fe81c20d892a
lucasccgomes <lucasccgomes@protonmail.com>
2024-08-21 08:45:53 -0300

    feat:style

--- painelHelp/src/components/Modal/Modal.jsx
+++ painelHelp/src/components/Modal/Modal.jsx
@@ -10,7 +10,7 @@ const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-50 overflow-auto bg-smoke-light flex">
+    <div className="fixed inset-0 z-50 bg-black/60 overflow-auto bg-smoke-light flex">
       <animated.div style={animation} className="relative p-8 bg-white w-full max-w-md m-auto flex-col flex rounded-lg shadow-lg">
         <span onClick={onClose} className="cursor-pointer absolute top-0 w-10 flex justify-center items-center h-10 right-0 p-4 b bg-red-600 rounded-full m-2">
           <button className='text-white font-bold'>X</button>
