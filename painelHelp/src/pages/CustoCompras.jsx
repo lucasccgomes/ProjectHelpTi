cf3ffa3e46a6aa6f9a79ac23caca5cc8957118fc
lucasccgomes <lucasccgomes@protonmail.com>
2024-08-30 10:08:56 -0300

    feat:new screen for purchasing sector

---
+++ painelHelp/src/pages/CustoCompras.jsx
@@ -0,0 +1,13 @@
+import RelatorioCusto from "../components/RelatorioCusto/RelatorioCusto";
+
+const CustoCompras = () => {
+
+
+    return (
+        <div className="bg-altBlue lg:h-screen">
+            <RelatorioCusto />
+        </div>
+    );
+};
+
+export default CustoCompras;
