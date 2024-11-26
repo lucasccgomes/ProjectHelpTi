d5932f16317643aa44a1599e8d6e69c8770f4a22
lucasccgomes <lucasccgomes@protonmail.com>
2024-11-26 09:56:56 -0300

    feat(marketing-tickets): adicionar gerenciador de chamados para administração
    - Criado componente 'MkGerenciadorChamados' como contêiner para gerenciamento de chamados.
    - Importado e integrado o componente 'MkAdmChamados'.
    - Configurado o layout básico com classe 'bg-altBlue' e altura responsiva para tela inteira.

---
+++ painelHelp/src/pages/MkGerenciadorChamados.jsx
@@ -0,0 +1,14 @@
+import MkAdmChamados from "../components/MkAdmChamados/MkAdmChamados";
+
+
+const MkGerenciadorChamados = () => {
+
+
+    return (
+        <div className="bg-altBlue lg:h-screen">
+            <MkAdmChamados />
+        </div>
+    );
+};
+
+export default MkGerenciadorChamados;
